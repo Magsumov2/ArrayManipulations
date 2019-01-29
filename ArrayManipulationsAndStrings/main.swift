9ea78d0fea324f720d22e153ff690b3e7e282c1a
муса магсумов <musamagsumov2@gmail.com>
2019-01-29 08:45:20 +0300

    Initial Commit

---
+++ ArrayManipulationsAndStrings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ArrayManipulationsAndStrings
+//
+//  Created by муса магсумов on 29/01/2019.
+//  Copyright © 2019 муса магсумов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
